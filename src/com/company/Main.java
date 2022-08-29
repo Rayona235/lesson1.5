5f4c6b6da75f6dab36d7bffa4fe95b0f22d612d0
unknown <atabekovarayona@gmail.com>
2022-08-29 19:22:43 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,98 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello GeekTech");
+        int[] agesMyStudents = {14,17,18,15,16,14,15,13,16,16};
+        System.out.println("Kamola" + agesMyStudents[2]);
+        System.out.println("Now Akniet is" + agesMyStudents[7]);
+
+        agesMyStudents[7] = 13;
+        System.out.println( "After 2 mouths Akniet is" +agesMyStudents[7]);
+
+        double[] nums = {1.0 , 0.9, 0.7, 0.4, 0.8, 9.0, 5.0, -5.0, -0.9, -4.0, -0.3, -8.0, -8.0, 2.0 -9.0, };
+
+        //ДЗ
+        int[] number1 = { 7, 8, 5, 4, 1};
+
+        int[] number2 = {9, 6, 3, 2, 0};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
